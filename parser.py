29a13607987eef20c31e52babe77800422ebe582
Prokop Zavada <pzavada@c1r10s4.42prague.com>
2026-03-31 14:45:38 +0200

    parser modified, call file

--- parser.py
+++ parser.py
@@ -1,9 +1,32 @@
+import sys
+import os
+
+
 def read_config() -> dict:
     res = {}
-    with open('config.txt', 'r') as file:
-        for line in file:
-            if not line or line.startswith("#"):
-                continue
-            key, value = line.strip().split("=")
-            res[key] = value
-    return res
+    if len(sys.argv) != 2:
+        print("No valid configuration file, please try again!")
+    elif os.path.isfile(sys.argv[1]):
+        with open(sys.argv[1], 'r') as file:
+            for line in file:
+                if not line or line.startswith("#"):
+                    continue
+                if '=' in line:
+                    key, value = line.strip().split("=")
+                    res[key] = value
+                else:
+                    print("No valid configuration file, please try again!")
+                    return None
+            try:
+                res['WIDTH'] = int(res['WIDTH'])
+                res['HEIGHT'] = int(res['HEIGHT'])
+                res['ENTRY'] = int(res['ENTRY'].split(","))
+                res['EXIT'] = (int(res['EXIT'].split(",")))
+            except (ValueError, KeyError) as e:
+                print(e)
+        return res
+    else:
+        print("No valid configuration file, please try again!")
+
+
+print(read_config())
