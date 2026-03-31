793caa7439db9481bde93b32bef27c6e5f2f6832
Prokop Zavada <pzavada@c1r10s4.42prague.com>
2026-03-31 15:47:43 +0200

    modified err messages

--- parser.py
+++ parser.py
@@ -23,23 +23,29 @@ def read_config() -> dict:
 
                 exit_err = res['ENTRY'].split(",")
                 if len(exit_err) != 2:
-                    raise ValueError("Invalid configuration file")
+                    raise ValueError(f"ENTRY: {exit_err}")
                 else:
                     res['ENTRY'] = (int(exit_err[0]), int(exit_err[1]))
                
                 exit_err = res['EXIT'].split(",")
                 if len(exit_err) != 2:
-                    raise ValueError("Invalid configuration file")
+                    raise ValueError(f"EXIT: {exit_err}")
                 else:
                     res['EXIT'] = (int(exit_err[0]), int(exit_err[1]))
 
                 bool_map = {"TRUE": True, "FALSE": False}
                 res['PERFECT'] = bool_map[res['PERFECT'].upper()]
+            except KeyError as e:
+                print(f"Configuration Error: Missing or misspelled mandatory key {e}")
+                return None
+            except ValueError as e:
+                print(f"Configuration Error: Invalid data format or value {e}")
             except Exception as e:
-                print(e)
+                print(f"An unxepected error occured: {e}")
                 return None
         return res
     else:
         print("No valid configuration file, please try again!")
 
 
+read_config()
