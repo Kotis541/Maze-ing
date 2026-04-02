298119e0dfc3edeed157206c7392f5a846fdcb26
Popoz123 <popozavada@gmail.com>
2026-04-02 16:31:07 +0200

    path finding updated

--- backend.py
+++ backend.py
@@ -109,7 +109,19 @@ class MazeGenerator():
     def _calc_h(self, x1: int, x2: int, y1: int, y2: int) -> int:
         return abs(x1 - x2) + abs(y1 - y2)
 
+    def _valid_cells(self, x: int, y: int) -> list[list[int, int]]:
+        res: list[list[int, int]] = []
+        if self._maze[x][y][0] == 0:
+            res.append([x, y - 1])
+        if self._maze[x][y][1] == 0:
+            res.append([x + 1, y])
+        if self._maze[x][y][2] == 0:
+            res.append([x, y + 1])
+        if self._maze[x][y][3] == 0:
+            res.append([x - 1, y])
+        return res
+
     def find_path(self) -> list[list[int, int]]:
         open_set = [list(self._entry)]
         current = open_set[-1]
-        print(current)
+        
